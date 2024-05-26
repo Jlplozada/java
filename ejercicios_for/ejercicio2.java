d69cfc30d47244fea759cb43b61bdde7517c5dc7
Jlplozada <jose_lpaezL@soy.sena.edu.co>
2024-05-25 19:38:48 -0500

    ejercicio 2 de for

--- ejercicios_for/ejercicio2.java
+++ ejercicios_for/ejercicio2.java
@@ -7,15 +7,23 @@ import java.util.Scanner;
 
 public class ejercicio2 {
     public static void main(String[] args) {
-        Scanner teclado=new Scanner(System.in);
-    int n,f,altura,base,superficie;
-    n=0;
+    Scanner teclado=new Scanner(System.in);
+    int n,f;
+    float altura,base,superficie;
+    n=0;superficie=0;
+    System.out.print("numero de triangulos: ");
+    n=teclado.nextInt();
     for(f=1;f<=n;f++){
         System.out.println("ingrese datos de altura: ");
         altura=teclado.nextInt();
         System.out.println("ingrese datos de base: ");
         base=teclado.nextInt();
+        System.out.println("base es: "+base+" altura: "+altura+" superficie: "+base*altura/2);
+        if (base*altura/2>12) {
+            superficie+=1;
+        }
     }
+    System.out.println("la cantidad de superfiies mayor a 12 es: "+superficie);
     }
     
 }
