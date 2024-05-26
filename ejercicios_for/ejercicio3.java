10b1a1e30f4bdf467dbc16de6c3e7edae2543f68
Jlplozada <jose_lpaezL@soy.sena.edu.co>
2024-05-25 19:52:01 -0500

    por terminar

--- ejercicios_for/ejercicio3.java
+++ ejercicios_for/ejercicio3.java
@@ -9,7 +9,34 @@ import java.util.Scanner;
 public class ejercicio3 {
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
-
+        int f,n;
+        n=0;
+        float lado_a,lado_b,lado_c;
+        for (f=1;f<=n;f++){
+            System.out.print("Lado_a: ");
+            lado_a=teclado.nextFloat();
+            System.out.print("Lado_b: ");
+            lado_b=teclado.nextFloat();
+            System.out.print("Lado_c: ");
+            lado_c=teclado.nextFloat();
+            if (lado_a==lado_b) {
+                if (lado_b==lado_c) {
+                    System.out.println("los tres lados son iguales");
+                }
+                if (lado_c==lado_a) {
+                    
+                }
+                if (lado_b==lado_a) {
+                    
+                }
+            }
+            if (lado_b==lado_c) {
+                
+            }
+            else{
+                
+            }
+        }
     }
 }
 
